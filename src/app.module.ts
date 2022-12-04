775b0c625542e3a9f2e552e245f86478a3cc8176
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-12-03 21:32:28 -0300

    add: prisma service models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //   definitions: {
+    //     path: join(process.cwd(), 'src/graphql.ts'),
+    //   },
+    // }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
