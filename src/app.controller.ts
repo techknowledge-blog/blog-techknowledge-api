4e7a0e5ad6ff66a3bdd3216c94e320bb7d76af93
Mikkaiser <mikaelrsimoes19@gmail.com>
2023-04-12 00:38:20 -0300

    chore: :fire: remove .env file and remove unnecessary lines on app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { PostsModule } from './modules/posts/posts.module';
-import { PostsService } from './modules/posts/posts.service';
-
 @Controller()
 export class AppController {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor(private readonly postService: PostsService) {}
-
-  @Get()
-  getPosts(): Promise<PostsModule[]> {
-    return this.postService.posts({ where: { isPublished: true } });
-  }
+  constructor() {}
 }
