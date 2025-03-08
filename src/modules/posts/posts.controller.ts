054481cebe94e07fe55ebef1c1185b69508790d6
Leticia Dias <contatoleticiadia@gmail.com>
2025-03-08 17:28:10 -0300

    refactor(posts): renamed function names and changed routes

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,25 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { Post as PostModel } from '@prisma/client';
 
-@Controller()
+@Controller('posts')
 export class PostsController {
   constructor(private readonly postService: PostsService) {}
 
-  @Get('post/:id')
-  async getPostById(@Param('id') id: string): Promise<PostModel> {
-    return this.postService.post({ id: Number(id) });
-  }
-
-  @Get('feed')
-  async getPublishedPosts(): Promise<PostModel[]> {
+  @Get('published')
+  async findPublishedPosts(): Promise<PostModel[]> {
     return this.postService.posts({
       where: { isPublished: true },
     });
   }
 
   @Get('filtered-posts/:searchString')
-  async getFilteredPosts(
+  async findFilteredPosts(
     @Param('searchString') searchString: string,
   ): Promise<PostModel[]> {
     return this.postService.posts({
@@ -36,28 +31,10 @@ export class PostsController {
     });
   }
 
-  // @Post('post')
-  // async createDraft(@Body() postData: CreatePostDto): Promise<PostModel> {
-  //   return this.postService.createPost(postData);
-  // }
-
-  // @Post('user')
-  // async signupUser(
-  //   @Body() userData: Prisma.UserCreateInput,
-  // ): Promise<UserModel> {
-  //   return this.userService.createUser(userData);
-  // }
-
-  // @Put('publish/:id')
-  // async publishPost(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.updatePost({
-  //     where: { id: Number(id) },
-  //     data: { isPublished: true },
-  //   });
-  // }
-
-  // @Delete('post/:id')
-  // async deletePost(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.deletePost({ id: Number(id) });
-  // }
+  @Get(':id')
+  async findPostById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<PostModel> {
+    return this.postService.post({ id });
+  }
 }
