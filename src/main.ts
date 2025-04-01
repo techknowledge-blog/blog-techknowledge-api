bc429c0328e1586fd35bba60452c224719d2d6fe
Mikkaiser <mikaelrsimoes19@gmail.com>
2025-04-01 22:13:05 +0400

    add: cors configs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  dotenv.config();
+  app.enableCors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
