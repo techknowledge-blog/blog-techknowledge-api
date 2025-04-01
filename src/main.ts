e7deab1ab794ce32ffbfce65d8a6dcd74257834e
Mikkaiser <mikaelrsimoes19@gmail.com>
2025-04-01 23:14:42 +0400

    add: test2

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    // credentials: true
+    credentials: true
   });
   await app.listen(3000);
 }
