3dbb1e1a7510ce57d8396f229a014f838ad7d49e
Leticia Dias <contatoleticiadia@gmail.com>
2025-07-29 21:30:11 -0300

    feat: created CreateUserDto for data validation and typing

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsUrl()
+  avatarLink?: string;
+
+  @IsOptional()
+  @IsString()
+  passwordHash?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  function: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
