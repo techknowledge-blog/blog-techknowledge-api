f2fadebc1a2a102b5587a99ed066f9560f819582
Leticia Dias <contatoleticiadia@gmail.com>
2025-08-11 21:46:00 -0300

    feat(auth): created the authenticate, validateUser and signIn methods

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,38 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcryptjs';
-import { PrismaService } from 'src/prisma.service';
+
+type AuthInput = { username: string; password: string };
+type SignInData = { userId: number; username: string; role: string };
+type AuthResult = {
+  accessToken: string;
+  userId: number;
+  username: string;
+  role: string;
+};
 
 @Injectable()
 export class AuthService {
   constructor(
+    private usersService: UsersService,
     private jwtService: JwtService,
-    private prismaService: PrismaService,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.prismaService.user.findFirst({
-      where: { name: username },
-    });
+  async authenticate(input: AuthInput): Promise<AuthResult> {
+    const user = await this.validateUser(input);
 
     if (!user) {
-      return null;
+      throw new UnauthorizedException();
     }
 
-    // const isMatch = await bcrypt.compare(password, user.passwordHash);
-    const isMatch = password == user.passwordHash;
-    if (isMatch) {
-      const { passwordHash, ...result } = user;
-      return result;
+    return this.signIn(user);
+  }
+
+  async validateUser(input: AuthInput): Promise<SignInData | null> {
+    const user = await this.usersService.findUserByName(input.username);
+
+    if (user && user.passwordHash === input.password) {
+      return {
+        userId: user.id,
+        username: user.name,
+        role: user.role,
+      };
     }
 
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.name, sub: user.id, role: user.role };
+  async signIn(user: SignInData): Promise<AuthResult> {
+    const tokenPayload = {
+      sub: user.userId,
+      username: user.username,
+      role: user.role,
+    };
+
+    const accessToken = await this.jwtService.signAsync(tokenPayload);
+
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken,
+      username: user.username,
+      userId: user.userId,
+      role: user.role,
     };
   }
 }
